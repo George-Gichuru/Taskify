c75ceffc434fadcd4784fd9f15f984323fba87b2
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2023-11-23 19:09:23 +0300

    Authentication

---
+++ app/(platform)/protected/page.tsx
@@ -0,0 +1,12 @@
+import {UserButton} from "@clerk/nextjs"
+
+const ProtectedPage = () => {
+    
+    return (
+        <div>
+           <UserButton afterSignOutUrl="/"/>
+        </div>
+    )
+}
+
+export default ProtectedPage
