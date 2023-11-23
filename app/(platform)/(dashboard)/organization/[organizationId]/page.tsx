cc335c8db17359f5249d38e8d4ab32eefc357d0b
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2023-11-23 22:56:42 +0300

    Organizations

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,10 @@
+
+const OrganizationPage = () => {
+    return (
+        <div>
+            Organzation page - this is where our organizations will be located
+        </div>
+    )
+}
+
+export default OrganizationPage
